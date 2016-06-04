20a72d0691d84060d36ca9f21a76ddae187b72a7
Goran Bogic <goran.bogic@infokom.hr>
2016-06-04 15:39:00 +0200

    Dodan server side u Flasku

---
+++ server/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, jsonify, request, render_template, abort
+
+# setting up static directory
+STATIC_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + '/client'
+
+app = Flask(__name__, static_folder=STATIC_DIR)
+app.debug = True
+
+# Routes
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    return app.send_static_file(path)
+
+# Generate object-database mapping
+
+# Register views
+
+if __name__ == '__main__':
+    # Make sure each thread gets a db session
+    app.run(port=3000)
